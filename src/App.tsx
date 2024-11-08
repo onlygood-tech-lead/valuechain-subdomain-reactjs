e49b3680410f51a9e2295094805a3d43290e9200
Onlygood Tech Lead <techlead@onlygood.ai>
2024-11-08 23:59:03 +0530

    fix: wrapped app <div> with ThemeContext for theme to be applied
    - tmp: added ThemeToggleButton to test theme values

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import AppContainer from "./components/containers/AppContainer";
 import FaqsSection from "./components/faqs/FaqsSection";
+import { ThemeProvider } from "./hooks/ThemeContext";
+import ThemeToggleButton from "./components/buttons/ThemeToggleButton";
 
 export default function App() {
   return (
-    <AppContainer>
-      <FaqsSection />
-    </AppContainer>
+    <ThemeProvider>
+      <AppContainer>
+        <FaqsSection />
+        <ThemeToggleButton />
+      </AppContainer>
+    </ThemeProvider>
   );
 }
